e1dbd0774be6c00254e2b2646a0564d0a7464637
peacebaker <116932680+peacebaker@users.noreply.github.com>
2024-07-30 21:29:44 -0400

    template script

---
+++ template.js
@@ -0,0 +1,11 @@
+/**
+ * Import type definitions from bitburner source.
+ * @ts-check
+ * @typedef {import('./bitburner-src/src/ScriptEditor/NetscriptDefinitions').NS} NS
+ */
+
+
+/** @param {NS} ns */
+export async function main(ns) {
+
+}
